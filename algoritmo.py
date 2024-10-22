f01221da912d975d1d34f01c8a58bdd64e323e97
JaredUNACH <jared.salazar65@unach.mx>
2024-10-22 11:10:03 -0600

    DDA circulo implementado en Python Matplotlib

--- algoritmo.py
+++ algoritmo.py
@@ -1,62 +1,96 @@
 import matplotlib.pyplot as plt
 
-def plot_circle_points(Xc, Yc, x, y, puntos):
-    puntos.append((Xc + x, Yc + y))
-    puntos.append((Xc - x, Yc + y))
-    puntos.append((Xc + x, Yc - y))
-    puntos.append((Xc - x, Yc - y))
-    puntos.append((Xc + y, Yc + x))
-    puntos.append((Xc - y, Yc + x))
-    puntos.append((Xc + y, Yc - x))
-    puntos.append((Xc - y, Yc - x))
+def plot_circle_points(Xc, Yc, x, y, octantes):
+    octantes[0].append((x, y))
+    octantes[1].append((-x, y))
+    octantes[2].append((x, -y))
+    octantes[3].append((-x, -y))
+    octantes[4].append((y, x))
+    octantes[5].append((-y, x))
+    octantes[6].append((y, -x))
+    octantes[7].append((-y, -x))
 
 def midpoint_circle_algorithm(Xc, Yc, r):
     x = 0
     y = r
     p = 1 - r
-    puntos = []
+    puntos = [(x, y, p)]
+    octantes = [[] for _ in range(8)]
 
-    plot_circle_points(Xc, Yc, x, y, puntos)
+    plot_circle_points(Xc, Yc, x, y, octantes)
 
     while x < y:
         x += 1
         if p < 0:
-            p += 2 * x + 1
+            p = p + 2 * x + 1
         else:
             y -= 1
-            p += 2 * x + 1 - 2 * y
-        plot_circle_points(Xc, Yc, x, y, puntos)
+            p = p + 2 * x + 1 - 2 * y
+        puntos.append((x, y, p))
+        plot_circle_points(Xc, Yc, x, y, octantes)
 
-    return puntos
+    return puntos, octantes
 
-def fill_circle(Xc, Yc, r):
-    puntos = midpoint_circle_algorithm(Xc, Yc, r)
-    puntos_relleno = set(puntos)
+def imprimir_tablas(puntos, Xc, Yc):
+    print("N\tPk\tXk+1\tYk-1")
+    for i, (x, y, p) in enumerate(puntos):
+        if i == 0:
+            print(f"{i}\t{p}\t{x + 1}\t{y}")
+        else:
+            if puntos[i-1][2] < 0:
+                print(f"{i}\t{p}\t{x + 1}\t{y}")
+            else:
+                print(f"{i}\t{p}\t{x + 1}\t{y - 1}")
 
-    for y in range(Yc - r, Yc + r + 1):
-        puntos_en_y = [p for p in puntos if p[1] == y]
-        if len(puntos_en_y) > 1:
-            x_min = min(p[0] for p in puntos_en_y)
-            x_max = max(p[0] for p in puntos_en_y)
-            for x in range(x_min, x_max + 1):
-                puntos_relleno.add((x, y))
+    print("\nY\tX")
+    for x, y, _ in puntos:
+        print(f"{y}\t{x}")
 
-    return sorted(puntos_relleno)
+    print("\nX\t-Y")
+    for x, y, _ in puntos:
+        print(f"{x}\t{-y}")
 
-# Parámetros del círculo
-Xc, Yc = 150, 100
-r = 10
+    print("\n-Y\tX")
+    for x, y, _ in puntos:
+        print(f"{-y}\t{x}")
 
-# Obtener los puntos del círculo relleno
-puntos_circulo = fill_circle(Xc, Yc, r)
+    print("\n-Y\t-X")
+    for x, y, _ in puntos:
+        print(f"{-y}\t{-x}")
 
-# Imprimir los puntos del círculo
-print(f"Puntos del círculo: {puntos_circulo}")
+    print("\n-X\t-Y")
+    for x, y, _ in puntos:
+        print(f"{-x}\t{-y}")
+
+    print("\n-X\tY")
+    for x, y, _ in puntos:
+        print(f"{-x}\t{y}")
+
+    print("\nY\t-X")
+    for x, y, _ in puntos:
+        print(f"{y}\t{-x}")
+
+def graficar_circulo(puntos, Xc, Yc):
+    x_vals = []
+    y_vals = []
+    for x, y, _ in puntos:
+        x_vals.extend([Xc + x, Xc - x, Xc + x, Xc - x, Xc + y, Xc - y, Xc + y, Xc - y])
+        y_vals.extend([Yc + y, Yc + y, Yc - y, Yc - y, Yc + x, Yc + x, Yc - x, Yc - x])
+
+    plt.scatter(x_vals, y_vals, s=1)
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.show()
+
+# Solicitar al usuario los parámetros del círculo
+Xc = int(input("Ingrese la coordenada X del centro del círculo: "))
+Yc = int(input("Ingrese la coordenada Y del centro del círculo: "))
+r = int(input("Ingrese el radio del círculo: "))
+
+# Obtener los puntos del círculo y los octantes
+puntos, octantes = midpoint_circle_algorithm(Xc, Yc, r)
+
+# Imprimir las tablas de los puntos calculados
+imprimir_tablas(puntos, Xc, Yc)
 
 # Graficar el círculo
-x_vals = [p[0] for p in puntos_circulo]
-y_vals = [p[1] for p in puntos_circulo]
-
-plt.scatter(x_vals, y_vals, s=1)
-plt.gca().set_aspect('equal', adjustable='box')
-plt.show()
+graficar_circulo(puntos, Xc, Yc)
